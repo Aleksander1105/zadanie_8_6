5bce1ffb15d0a7037ad55b5b84a600e6c250f17e
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-02-24 12:54:26 +0100

    Zadanie_8_6-done

---
+++ script.js
@@ -0,0 +1,17 @@
+var a = 2; 
+var b = 4;
+var value = (a * a) - (2 * a * b) - (b * b);
+
+if
+ (value > 0) {
+    console.log('Wynik dodatni: ' + value);
+} else {
+    console.log('Wynik ujemny: ' + value);
+}
+
+if
+ (value = 0) {
+    console.log('Wynik równy ' + 0);
+} else {
+    console.log('Wynik różny od ' + 0);
+}
