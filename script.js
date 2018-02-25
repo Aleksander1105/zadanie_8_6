e068f1e8f45b41577c3c03dd6e40b894feadf557
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-02-25 23:42:32 +0100

    Modified script.js as requested

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ var a = 2;
 var b = 4;
 var value = (a * a) - (2 * a * b) - (b * b);
 
-if
- (value > 0) {
+if (value > 0) {
     console.log('Wynik dodatni: ' + value);
 } else {
     console.log('Wynik ujemny: ' + value);
 }
 
-if
- (value = 0) {
+if (value == 0) {
     console.log('Wynik równy ' + 0);
 } else {
     console.log('Wynik różny od ' + 0);
